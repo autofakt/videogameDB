0bd8547cd3688f20c5639ec6b5074377cfee01f4
Mike Be <autofakt5@gmail.com>
2021-04-11 20:19:13 -0700

    Initial commit

---
+++ videogameDB.py
@@ -0,0 +1,12 @@
+from tkinter import Tk,Button,Label,Scrollbar,Listbox,StringVar,Entry,W,E,N,S,END
+from tkinter import ttk
+from tkinter import messagebox
+
+root = Tk()
+
+root.title("Video Game Pro")
+root.configure(background="light blue")
+root.geometry("1050x650")
+root.resizable(width=False,height=False)
+
+root.mainloop()
